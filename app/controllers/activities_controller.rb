ece2beaf583746a162280d242842ad8cc5af9174
springHeeledHack <kylemclellan@gmail.com>
2019-05-07 10:59:29 -0400

    got a CRD app going... needs the U

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -12,4 +12,30 @@ class ActivitiesController < ApplicationController
     activity.save
     redirect '/'
   end
+
+  get '/activities/end/:id' do
+    activity = Activity.find_by(id: params[:id])
+    if activity.user_id == current_user.id
+      activity.end_time = Time.now
+      activity.save
+    end
+    redirect '/'
+  end
+
+  get '/activities/resume/:id' do
+    resume = Activity.find_by(id: params[:id])
+    activity = Activity.new(name: resume.name)
+    activity.user_id = current_user.id
+    activity.start_time = Time.now
+    activity.save
+    redirect '/'
+  end
+
+  get '/activities/delete/:id' do
+    activity = Activity.find_by(id: params[:id])
+    if activity.user_id == current_user.id
+      activity.destroy
+    end
+    redirect '/'
+  end
 end
