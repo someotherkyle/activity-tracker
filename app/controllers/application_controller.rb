b3e9b10a8034d2c9fda5e1b9c6eaefdfaba2ff0d
springHeeledHack <kylemclellan@gmail.com>
2019-05-07 23:15:38 -0400

    pedantic double quote to single quote ¯\_(ツ)_/¯

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < Sinatra::Base
 
   configure do
     enable :sessions
-    set :session_secret,ENV["SESSION_SECRET"]
+    set :session_secret,ENV['SESSION_SECRET']
     set :public_folder, 'public'
     set :views, 'app/views'
   end
